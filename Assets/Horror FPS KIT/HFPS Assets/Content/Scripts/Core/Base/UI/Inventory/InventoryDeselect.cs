7d150f531e9549f2d4220532f97973ff6d3679bd
Noor Ali Butt <noor@crosscode.dev>
2023-07-24 19:00:53 +0500

    another partial commit

---
+++ Assets/Horror FPS KIT/HFPS Assets/Content/Scripts/Core/Base/UI/Inventory/InventoryDeselect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using HFPS.Systems;
+
+namespace HFPS.UI
+{
+    public class InventoryDeselect : MonoBehaviour, IPointerClickHandler
+    {
+        private Inventory inventory;
+        private HFPS_GameManager gameManager;
+
+        void Awake()
+        {
+            inventory = transform.root.GetComponentInChildren<Inventory>();
+
+            if (HFPS_GameManager.HasReference)
+            {
+                gameManager = HFPS_GameManager.Instance;
+            }
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            inventory.ResetInventory();
+
+            gameManager.OnInventory();
+        }
+    }
+}
