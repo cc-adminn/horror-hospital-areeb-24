6f0e7613812d2316987a16b82854beaaf3e4750c
Noor Ali Butt <noor@crosscode.dev>
2023-08-19 03:50:06 +0500

    adds app tracking transparency request

---
+++ Assets/FPS Adapter/Scripts/AppTrackingTransparencyRequest.cs
@@ -0,0 +1,30 @@
+#if UNITY_IOS && EM
+using EasyMobile;
+#endif
+using UnityEngine;
+
+public class AppTrackingTransparencyRequest : MonoBehaviour
+{
+    private void Start()
+    {
+    #if UNITY_IOS && EM
+        var previousStatus = Privacy.AppTrackingManager.TrackingAuthorizationStatus;
+
+        if (previousStatus == AppTrackingAuthorizationStatus.ATTrackingManagerAuthorizationStatusNotDetermined)
+        {
+            Privacy.AppTrackingManager.RequestTrackingAuthorization(status =>
+            {
+                Debug.Log("App Tracking transparency status: " + status);
+                
+                // If the user opts out of targeted advertising:
+                var gdprMetaData = new MetaData("gdpr");
+                
+                gdprMetaData.Set("consent", 
+                    status == AppTrackingAuthorizationStatus.ATTrackingManagerAuthorizationStatusAuthorized 
+                        ? "true" : "false");
+                Advertisement.SetMetaData(gdprMetaData);
+            });
+        }
+    #endif
+    }
+}
